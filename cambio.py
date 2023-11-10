0f28c803167913ad1f1a15d217373645063d9549
Familia Alvarez <malvarez660@unab.edu.co>
2023-11-10 14:26:24 -0500

    version casi ultima

--- cambio.py
+++ cambio.py
@@ -1,78 +1,42 @@
+from flask import Flask, render_template, request, redirect, url_for, session
 import requests
-import json
-import matplotlib.pyplot as plt
 
-# API de cambio de moneda
-url = "https://api.currencyfreaks.com/v2.0/rates/latest?apikey=e2277eb0e898401e839242d24c90833d&symbols=EUR,GBP,COP,ARS,MXN,PKR,INR,CAD,AUD,AED,ILS,RUB,CHF,JPY,CNY,BRL&base=USD"
+app = Flask(__name__, template_folder='templates')
+app.secret_key = 'e2277eb0e898401e839242d24c90833d'
+API_KEY = 'e2277eb0e898401e839242d24c90833d'
 
-# Monedas disponibles
-MONEY_CODES = {
-    "COP":"Peso colombiano",
-    "ARS":"Peso argentino",
-    "MXN":"Peso mexicano",
-    "PKR":"Rupia pakistaní",
-    "INR":"Rupia india",
-    "AED":"Dírham de los Emiratos Árabes Unidos",
-    "ILS":"Nuevo séquel",
-    "RUB":"Rublo ruso",
-    "EUR": "Euro",
-    "USD": "Dólar estadounidense",
-    "GBP": "Libra esterlina",
-    "JPY": "Yen japonés",
-    "CHF": "Franco suizo",
-    "CAD": "Dólar canadiense",
-    "AUD": "Dólar australiano",
-    "CNY": "Yuan chino",
-    "BRL": "Real brasileño",
-}
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-def get_exchange_rates():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('dashboard'))
+    return render_template('/templates/login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+@app.route('/dashboard')
+def dashboard():
+    if 'username' in session:
+        return render_template('dashboard.html', username=session['username'])
+    return redirect(url_for('login'))
+
+@app.route('/convert', methods=['POST'])
+def convert_currency():
+    amount = request.form['amount']
+    from_currency = request.form['from_currency']
+    to_currency = request.form['to_currency']
+    url = f'https://api.currencyfreaks.com/v2.0/rates/latest?apikey=e2277eb0e898401e839242d24c90833d&symbols=EUR,GBP,COP,ARS,MXN,PKR,INR,CAD,AUD,AED,ILS,RUB,CHF,JPY,CNY,BRL&base=USD'
     response = requests.get(url)
-    if response.status_code == 200:
-        return json.loads(response.content)
-    else:
-        return None
+    data = response.json()
+    result = data['result']
+    return render_template('result.html', result=result)
 
-# Función para convertir una cantidad de una moneda a otra
-def convert_currency(amount, from_currency, to_currency):
-    exchange_rates = get_exchange_rates()
-    if exchange_rates is None:
-        return None
-
-    from_rate = exchange_rates["rates"][from_currency]
-    to_rate = exchange_rates["rates"][to_currency]
-
-    return amount * (to_rate / from_rate)
-
-# Página web
-def main():
-    # Crear un formulario para obtener la cantidad y las monedas
-    amount = input("Cantidad a convertir: ")
-    from_currency = input("Moneda de origen: ")
-    to_currency = input("Moneda de destino: ")
-
-    # Obtener las tasas de cambio
-    exchange_rates = get_exchange_rates()
-    if exchange_rates is None:
-        print("Error al obtener las tasas de cambio.")
-        return
-
-    # Convertir la cantidad
-    converted_amount = convert_currency(float(amount), from_currency, to_currency)
-
-    # Mostrar el resultado
-    print(f"{amount} {MONEY_CODES[from_currency]} = {converted_amount} {MONEY_CODES[to_currency]}")
-
-    # Obtener los datos de las tasas de cambio
-    rates = exchange_rates["rates"]
-
-    # Crear la gráfica
-    plt.plot(rates.keys(), rates.values())
-    plt.xlabel("Moneda")
-    plt.ylabel("Tasa de cambio")
-
-    # Mostrar la gráfica
-    plt.show()
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(debug=True)
